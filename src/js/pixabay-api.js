3e70715d05ae40241621f9e25eb7bab68197701b
unknown <mais-nasinya@ukr.net>
2025-02-19 17:26:42 +0200

    first

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,15 @@
+export const fetchPhotos = searchedQuery => {
+  const params = new URLSearchParams({
+    key: '48862284-111645087e0508ab98b21d672',
+    q: searchedQuery,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+  return fetch(`https://pixabay.com/api/?${params}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+};
